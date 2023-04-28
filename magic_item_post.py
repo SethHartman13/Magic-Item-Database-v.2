c8305db9695b8185d78ed497272565c394ebf67c
SethHartman13 <familyhartseth@aol.com>
2023-04-28 00:27:40 -0600

    Proper clearing of error, connection error correct

--- magic_item_post.py
+++ magic_item_post.py
@@ -1,9 +1,12 @@
 # Import for datatyping
 from google.auth.transport.requests import AuthorizedSession
+import google.auth.transport.requests as requests
+from http import client
 
 # Built-in libraries
 import json
 import threading
+import time
 
 # Created modules
 import connection_error
@@ -37,66 +40,89 @@ class RequestThread(threading.Thread):
         self.index_lock = index_lock
         self.print_lock = print_lock
         self.error_lock = error_lock
+        self.connection_attempts = 0
 
     def run(self):
         global error_files
-
-        # Checks to see if the file has already been added to the index
-        with self.index_lock:
-            with open(self.index_json_dir, 'r') as f:
-                index_json = json.load(f)
-
-        # If the file is already in the index
-        if self.file_name in index_json.keys():
-
-            # Print lock
-            with self.print_lock:
-                print(f"{self.file_name} already exists! ")
-
-        # If the file is not already in the index
-        else:
-
-            # Builds file path
-            full_file_dir = f"{self.file_dir}/{self.file_name}"
-
-            # Opens JSON file (thread safe because threads are accessing different files)
-            with open(full_file_dir, 'r') as f:
-                json_file = f.read()
-
-            # Sends JSON to database
-            response = self.session.post(self.full_url, data=json_file)
-
-            # If the database says it was a good request
-            if response.status_code == 200:
-
-                # Puts response into a JSON
-                response_detail = response.json()
-
-                # Index lock
+        
+        while self.connection_attempts < 3:
+            
+            try:
+                
+                # Checks to see if the file has already been added to the index
                 with self.index_lock:
-
-                    # Pulls up latest version of index
                     with open(self.index_json_dir, 'r') as f:
                         index_json = json.load(f)
 
-                    # We add the added json name to the index file with the unique ID assigned by the db
-                    index_json[str(self.file_name)] = response_detail['name']
+                # If the file is already in the index
+                if self.file_name in index_json.keys():
 
-                    # Overwrites index JSON (with formatting)
-                    with open(self.index_json_dir, 'w') as f:
-                        json.dump(index_json, f, indent=4, sort_keys=True)
+                    # Print lock
+                    with self.print_lock:
+                        print(f"{self.file_name} already exists! ")
 
-                # Print lock
-                with self.print_lock:
-                    print(f"{self.file_name} successfully added!")
+                # If the file is not already in the index
+                else:
 
-            # If the databases says it was not a good request
+                    # Builds file path
+                    full_file_dir = f"{self.file_dir}/{self.file_name}"
+
+                    # Opens JSON file (thread safe because threads are accessing different files)
+                    with open(full_file_dir, 'r') as f:
+                        json_file = f.read()
+
+                    # Sends JSON to database
+                    response = self.session.post(self.full_url, data=json_file)
+
+                    # If the database says it was a good request
+                    if response.status_code == 200:
+
+                        # Puts response into a JSON
+                        response_detail = response.json()
+
+                        # Index lock
+                        with self.index_lock:
+
+                            # Pulls up latest version of index
+                            with open(self.index_json_dir, 'r') as f:
+                                index_json = json.load(f)
+
+                            # We add the added json name to the index file with the unique ID assigned by the db
+                            index_json[str(self.file_name)] = response_detail['name']
+
+                            # Overwrites index JSON (with formatting)
+                            with open(self.index_json_dir, 'w') as f:
+                                json.dump(index_json, f, indent=4, sort_keys=True)
+
+                        # Print lock
+                        with self.print_lock:
+                            print(f"{self.file_name} successfully added!")
+
+                    # If the databases says it was not a good request
+                    else:
+
+                        # Error lock
+                        with self.error_lock:
+                            error_files.append(
+                                f"{self.file_name} Error: {response.status_code}")
+                            
+            except client.RemoteDisconnected:
+                print(f"Connection failed with {self.file_name}")
+                self.connection_attempts += 1
+                
+                if self.connection_attempts >= 3:
+                    error_files.append(f"{self.file_name} Error: ConnectionError")
+                    break
+                    
+                else:
+                    time.sleep(3)
+                    pass
+                
             else:
-
-                # Error lock
-                with self.error_lock:
-                    error_files.append(
-                        f"{self.file_name} Error: {response.status_code}")
+                break
+                
+                
+                
 
 
 def main(auth_session: AuthorizedSession, file_dir: str, index_json_dir: str, db_folder_url: str, file_list: list[str]) -> None:
@@ -110,6 +136,7 @@ def main(auth_session: AuthorizedSession, file_dir: str, index_json_dir: str, db
         db_folder_url (str): URL of target database folder
         file_list (list[str]): List of file names within target directory
     """
+    global error_files
 
     # Creates locks
     index_lock = threading.Lock()
@@ -137,6 +164,8 @@ def main(auth_session: AuthorizedSession, file_dir: str, index_json_dir: str, db
 
         for file in error_files:
             print(file)
+            
+        error_files = []
 
 if __name__ == "__main__":
     print("This code is not meant to be executed directly, please execute main.py instead.")
