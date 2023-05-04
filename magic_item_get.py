b58660faea170cfb7c9a4ab63d475914a93270e6
SethHartman13 <familyhartseth@aol.com>
2023-05-04 15:30:59 -0600

    Formatting and documentation

--- magic_item_get.py
+++ magic_item_get.py
@@ -1,11 +1,19 @@
-# Import for datatyping
+# Import for data-typing
 from google.auth.transport.requests import AuthorizedSession
 
-# Build in libraries
+# Build-in libraries
 import random
 
+# Global Constants
+MINI_DIVIDE = "---------------------------------------------------------\n"
+# ----------------------------------------------------------------------------------
 
-def main(auth_session: AuthorizedSession, full_URL: str, item_count: int) -> None:
+
+def main(
+    auth_session: AuthorizedSession, 
+    full_URL: str, 
+    item_count: int
+) -> None:
     """
     Main request function
 
@@ -14,6 +22,8 @@ def main(auth_session: AuthorizedSession, full_URL: str, item_count: int) -> Non
         full_URL (str): URL of database
         item_count (int): number of items desired
     """
+
+    # Sends response to the server to delete the json
     response = auth_session.get(full_URL)
 
     # If the database says it was a good request
@@ -87,46 +97,57 @@ def main(auth_session: AuthorizedSession, full_URL: str, item_count: int) -> Non
                     if attunement_type == "None":
                         information_list.append("Requires attunement\n")
 
+                    # If the special requirement is a class
                     elif attunement_type == "class":
                         c_class = item_dict["class"]
                         information_list.append(
                             f"Requires attunement by a creature who has a least one level in the following class(es): {c_class}\n"
                         )
 
+                    # If the special requirement is a race
                     elif attunement_type == "race":
                         race = item_dict["race"]
                         information_list.append(
                             f"Requires attunement by the following race(s): {race}\n"
                         )
 
+                    # If the special requirement is something special
                     elif attunement_type == "other":
                         other = item_dict["other"]
                         information_list.append(f"Attunement Requirement: {other}\n")
 
+                    # This is to ensure that a JSON error can be corrected
                     else:
                         assert (
                             False
                         ), f"attunement_type was given an unknown attunement of {attunement_type} with item {name}"
 
+                # If the item does not require attunement
                 else:
                     information_list.append("")
 
+                # Appends details to the list
                 information_list.append(details)
 
-                if homebrew:
-                    information_list.append("HOMEBREW ITEM\n")
-                else:
+                # If the item is not homebrew
+                if not homebrew:
                     information_list.append("")
+                
+                # If the item is homebrew
+                else:
+                    information_list.append("HOMEBREW ITEM\n")
 
+            # Prints all the information for an item
             for line in information_list:
                 print(line)
 
+            # Makes it so that there is a break between items, but not at the end
             counter += 1
-
             if counter <= item_count - 1:
-                print("----------------------------------------------------------\n")
+                print(MINI_DIVIDE)
 
 
+# If the program is run directly when it is not supposed to 
 if __name__ == "__main__":
     print(
         "This code is not meant to be executed directly, please execute main.py instead."
